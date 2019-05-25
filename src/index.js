e3f2389760f9675f265adba3282f5a05751cb97a
RaneWallin <rane@ranewallin.com>
2019-05-25 10:28:50 -0700

    scaffold in redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { createStore } from "redux";
+import { Provider } from 'react-redux';
 import App from './components/App';
+import { cellReducer } from './reducers';
 
-ReactDOM.render(<App/>, document.querySelector('#root'));
+const store = createStore(cellReducer);
+
+
+
+ReactDOM.render(<Provider store={store}><App/></Provider>, document.querySelector('#root'));
---
+++ src/reducers/index.js
@@ -0,0 +1,3 @@
+export const cellReducer = (state=[], action) => {
+    return state;
+}
